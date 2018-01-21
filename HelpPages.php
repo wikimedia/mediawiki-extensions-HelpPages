2d745153f140a9916ac15a796470d1b5e39321d5
Jayprakash12345 <0freerunning@gmail.com>
2018-01-21 08:24:51 +0000

    Convert HelpPages to use extension registration
    Bug: T185407
    Change-Id: I32eb35853001c8f24f4025fa20bb00c2d25200f7

--- HelpPages.php
+++ HelpPages.php
@@ -12,40 +12,16 @@
  * @license Public domain
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	exit;
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'HelpPages' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['HelpPages'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the HelpPages extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the HelpPages extension requires MediaWiki 1.29+' );
 }
-
-/**
- * How long to cache the rendered HTML for
- *
- * default is one week
- */
-$wgHelpPagesExpiry = 60 * 60 * 24 * 7;
-
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'HelpPages',
-	'author' => 'Kunal Mehta',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:HelpPages',
-	'descriptionmsg' => 'helppages-desc',
-	'version' => '0.3.0',
-);
-
-$dir = dirname(__FILE__);
-
-$wgAutoloadClasses['HelpPages'] = $dir . '/HelpPages.body.php';
-$wgAutoloadClasses['HelpPagesHooks'] = $dir . '/HelpPages.hooks.php';
-
-$wgMessagesDirs['HelpPages'] = __DIR__ . '/i18n';
-
-$wgHooks['ShowMissingArticle'][] = 'HelpPagesHooks::onShowMissingArticle';
-$wgHooks['SkinTemplateNavigation::Universal'][] = 'HelpPagesHooks::onSkinTemplateNavigationUniversal';
-$wgHooks['ArticlePurge'][] = 'HelpPagesHooks::onArticlePurge';
-$wgHooks['LinkBegin'][] = 'HelpPagesHooks::onLinkBegin';
-
-$wgResourceModules['ext.HelpPages'] = array(
-	'styles' => 'ext.HelpPages.css',
-	'localBasePath' => $dir,
-	'remoteExtPath' => 'HelpPages',
-);
